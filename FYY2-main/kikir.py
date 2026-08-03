d6757e2cefb9ae7c11aeb89bbc68dad6c9b8ae49
IKOO-INC <muhammaddarjuni76@guru.smp.belajar.id>
2026-08-04 00:05:13 +0700

    Delete FYY2-main directory

--- FYY2-main/kikir.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,redirect, render_template, send_from_directory
-app = Flask(__name__)
-
-@app.route('/')
-def bsdchdf():
-    return send_from_directory('static','ru.png', as_attachment=True)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True)
